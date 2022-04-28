3c4d676a1520024ff358297482f7787d801d2e86
danistunaligil <danistunaligil@outlook.com>
2022-04-28 12:47:16 +0300

    anasayfa veri gösterme işlemleri, durum kontrol ve renklendirme yapıldı

--- kütüphane/anaSayfa.Designer.cs
+++ kütüphane/anaSayfa.Designer.cs
@@ -34,9 +34,21 @@
             this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.tsbHangiKayıtlar = new System.Windows.Forms.ToolStripComboBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnİşlem = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.toolStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // toolStrip1
@@ -45,11 +57,14 @@
             this.toolStripButton1,
             this.toolStripButton2,
             this.toolStripSeparator1,
-            this.toolStripButton3});
+            this.toolStripButton3,
+            this.toolStripSeparator2,
+            this.toolStripLabel1,
+            this.tsbHangiKayıtlar});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
-            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Size = new System.Drawing.Size(1031, 25);
+            this.toolStrip1.TabIndex = 1;
             this.toolStrip1.Text = "toolStrip1";
             // 
             // toolStripButton1
@@ -87,28 +102,146 @@
             this.toolStripButton3.Text = "Ayarlar";
             this.toolStripButton3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(60, 22);
+            this.toolStripLabel1.Text = "Görüntüle";
+            // 
+            // tsbHangiKayıtlar
+            // 
+            this.tsbHangiKayıtlar.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.tsbHangiKayıtlar.Items.AddRange(new object[] {
+            "Tümü",
+            "Süresi Dolanlar",
+            "Bugün Gelecekler",
+            "İadesi Yaklaşanlar"});
+            this.tsbHangiKayıtlar.Name = "tsbHangiKayıtlar";
+            this.tsbHangiKayıtlar.Size = new System.Drawing.Size(133, 25);
+            this.tsbHangiKayıtlar.SelectedIndexChanged += new System.EventHandler(this.tsbHangiKayıtlar_SelectedIndexChanged);
+            this.tsbHangiKayıtlar.Click += new System.EventHandler(this.tsbHangiKayıtlar_Click);
+            // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(90, 161);
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 49);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
-            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(1031, 401);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.ColumnSortModeChanged += new System.Windows.Forms.DataGridViewColumnEventHandler(this.dataGridView1_ColumnSortModeChanged);
+            this.dataGridView1.RowPrePaint += new System.Windows.Forms.DataGridViewRowPrePaintEventHandler(this.dataGridView1_RowPrePaint);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnİşlem);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(0, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1031, 24);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnİşlem
+            // 
+            this.btnİşlem.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnİşlem.Location = new System.Drawing.Point(656, 0);
+            this.btnİşlem.Name = "btnİşlem";
+            this.btnİşlem.Size = new System.Drawing.Size(145, 24);
+            this.btnİşlem.TabIndex = 2;
+            this.btnİşlem.Text = "Ödünç ver";
+            this.btnİşlem.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label4.Location = new System.Drawing.Point(601, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "                ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.textBox2.Location = new System.Drawing.Point(441, 0);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(160, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label2.Location = new System.Drawing.Point(370, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Barkod";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label3.Location = new System.Drawing.Point(315, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "                ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.textBox1.Location = new System.Drawing.Point(149, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(166, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tc Kimlik Numarası";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // anaSayfa
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Coral;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1031, 450);
             this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.toolStrip1);
             this.Name = "anaSayfa";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.anaSayfa_FormClosing);
             this.Load += new System.EventHandler(this.anaSayfa_Load);
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,5 +255,16 @@
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripButton toolStripButton3;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnİşlem;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripComboBox tsbHangiKayıtlar;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
     }
 }
--- kütüphane/anaSayfa.cs
+++ kütüphane/anaSayfa.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,6 +18,9 @@ namespace kütüphane
             InitializeComponent();
         }
 
+
+        MySqlConnection con = new MySqlConnection("Server=127.0.0.1;Database=kutuphane;Uid=root;Pwd=12345678;");
+
         private void anaSayfa_Load(object sender, EventArgs e)
         {
             DialogResult şifre = new Form1().ShowDialog();
@@ -25,6 +29,9 @@ namespace kütüphane
             System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
             Version version = assembly.GetName().Version;
             this.Text = String.Format("Kütüphane Programı {0}.{1}",version.Major,version.Minor);
+            con.Open();
+            tsbHangiKayıtlar.SelectedIndex = 0;
+            oduncVerilenleriGetir();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -40,6 +47,54 @@ namespace kütüphane
         private void button3_Click(object sender, EventArgs e)
         {
             new ayarlar().ShowDialog();
+        }
+
+        enum görüntüleme_tipi { süresiGeçenler=1,iadesiYaklaşanlar=3,bugünGelecekler=2,tümü=0}
+
+        void oduncVerilenleriGetir(görüntüleme_tipi hangiKayıtlar=görüntüleme_tipi.tümü)
+        {
+
+            string şartifadesi = "";
+            if (hangiKayıtlar == görüntüleme_tipi.süresiGeçenler)
+                şartifadesi = "WHERE odunc.getilecek_tarih < CURDATE()";
+            else if (hangiKayıtlar == görüntüleme_tipi.iadesiYaklaşanlar)
+                şartifadesi = "WHERE odunc.getilecek_tarih = CURDATE()+2 OR odunc.getilecek_tarih = CURDATE()+1";
+            else if (hangiKayıtlar == görüntüleme_tipi.bugünGelecekler)
+                şartifadesi = "WHERE odunc.getilecek_tarih = CURDATE()";
+
+            MySqlDataAdapter da = new MySqlDataAdapter("SELECT odunc.id,kitaplar.barkod AS 'Barkod',kitaplar.adi AS 'Kitap Adı',kitaplar.yazari AS 'Kitabın Yazarı',uyeler.tc AS 'TC',uyeler.ad AS 'Öğrenci Adı',uyeler.soyad AS 'Öğrenci Soyadı',uyeler.telefon AS 'Öğrenci Telefon',uyeler.e_posta AS 'Öğrenci E-Posta',odunc.verilis_tarihi AS 'Veriliş Tarihi',odunc.getilecek_tarih AS 'Getirilecek Tarih', if(odunc.getilecek_tarih < CURDATE(),1,if(odunc.getilecek_tarih = CURDATE(),2,if(odunc.getilecek_tarih = CURDATE()+1 OR odunc.getilecek_tarih = CURDATE()+2,3,0))) AS durum FROM(odunc INNER JOIN kitaplar ON odunc.kitap_id = kitaplar.id) INNER JOIN uyeler ON odunc.uye_id = uyeler.id " + şartifadesi, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            //dataGridView1.Columns[11].Visible = 
+            dataGridView1.Columns[0].Visible = false;
+            dataGridView1.Columns[10].DefaultCellStyle.Format = "dd.MM.yyyy";
+        }
+
+        private void anaSayfa_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            con.Close();
+        }
+
+        private void tsbHangiKayıtlar_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void tsbHangiKayıtlar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            oduncVerilenleriGetir((görüntüleme_tipi)tsbHangiKayıtlar.SelectedIndex);
+        }
+
+        private void dataGridView1_RowPrePaint(object sender, DataGridViewRowPrePaintEventArgs e)
+        {
+            int durum = Convert.ToInt16(dataGridView1.Rows[e.RowIndex].Cells[11].Value);
+            dataGridView1.Rows[e.RowIndex].DefaultCellStyle.BackColor = durum == 3 ? Color.LightYellow : (durum == 2 ? Color.Tomato : (durum == 1 ? Color.LightGray : Color.White));
+        }
+
+        private void dataGridView1_ColumnSortModeChanged(object sender, DataGridViewColumnEventArgs e)
+        {
+            
         }
     }
 }
